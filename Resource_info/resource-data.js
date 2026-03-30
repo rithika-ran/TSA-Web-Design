1fc22696e8a6a1a818d9bb1164ec5129c507ae45
rithika-ran <ritran29@bergen.org>
2026-03-30 16:14:39 -0400

    fixed map
    Co-authored-by: xinxie29-wq <xinxie29-wq@users.noreply.github.com>

---
+++ Resource_info/resource-data.js
@@ -0,0 +1,132 @@
+const resources = [
+  {
+    id: "greater-bergen-head-start",
+    name: "Greater Bergen - Head Start",
+    category: "Education",
+    address: "Bergen County, NJ",
+    lat: 40.8850,
+    lng: -74.0440,
+    page: "greaterbergenheadstart.html",
+    distanceMode: "fixed"
+  },
+  {
+    id: "educational-opportunity-fund",
+    name: "Educational Opportunity Fund (EOF)",
+    category: "Education",
+    address: "400 Paramus Rd, Paramus, NJ 07652",
+    lat: 40.9542,
+    lng: -74.0955,
+    page: "educationaloppurtunityfund.html",
+    distanceMode: "fixed"
+  },
+  {
+    id: "rutgers-4h-youth-development",
+    name: "Rutgers 4-H Youth Development",
+    category: "Education",
+    address: "Location varies by program",
+    lat: null,
+    lng: null,
+    page: "rutgers4-h.html",
+    distanceMode: "variable"
+  },
+  {
+    id: "ymca-greater-bergen",
+    name: "YMCA of Greater Bergen County",
+    category: "Extracurricular",
+    address: "1092 Carnation Dr, New Milford, NJ 07646",
+    lat: 40.9352,
+    lng: -74.0160,
+    page: "ymcaofgreaterbergencounty.html",
+    distanceMode: "fixed"
+  },
+  {
+    id: "bergen-community-college-childrens-enrichment",
+    name: "Bergen Community College - Children's Enrichment",
+    category: "Extracurricular",
+    address: "400 Paramus Rd, Paramus, NJ 07652",
+    lat: 40.9542,
+    lng: -74.0955,
+    page: "bergencommunitycollegechildrensenrichment.html",
+    distanceMode: "fixed"
+  },
+  {
+    id: "ywca-bergen-after-school",
+    name: "YWCA Northern New Jersey - Bergen After-School",
+    category: "Extracurricular",
+    address: "Bergen County, NJ",
+    lat: 40.8895,
+    lng: -74.0448,
+    page: "ywcanorthernnewjerseybergenafter-school.html",
+    distanceMode: "fixed"
+  },
+  {
+    id: "northwest-bergen-health-commission",
+    name: "Northwest Bergen Health Commission",
+    category: "Pediatrics",
+    address: "20 W Prospect St, Waldwick, NJ 07463",
+    lat: 41.0102,
+    lng: -74.1186,
+    page: "northwestbergenhealthcommission.html",
+    distanceMode: "fixed"
+  },
+  {
+    id: "bergens-promise",
+    name: "Bergen’s Promise (Care Management Organization)",
+    category: "Pediatrics",
+    address: "3 University Plaza Dr, Hackensack, NJ 07601",
+    lat: 40.8850,
+    lng: -74.0445,
+    page: "bergenspromise.html",
+    distanceMode: "fixed"
+  },
+  {
+    id: "careplus-nj-fqhc",
+    name: "CarePlus NJ (FQHC)",
+    category: "Pediatrics",
+    address: "365 W Passaic St, Suite 115, Rochelle Park, NJ 07662",
+    lat: 40.9077,
+    lng: -74.0706,
+    page: "careplusnjfqhc.html",
+    distanceMode: "fixed"
+  },
+  {
+    id: "bergen-county-division-community-transportation",
+    name: "Bergen County Division of Community Transportation",
+    category: "Transportation",
+    address: "One Bergen County Plaza, Hackensack, NJ 07601",
+    lat: 40.8855,
+    lng: -74.0436,
+    page: "bergencountydivisionofcommunitytransportation.html",
+    distanceMode: "fixed"
+  },
+  {
+    id: "modivcare-medical-transportation",
+    name: "Modivcare Medical Transportation",
+    category: "Transportation",
+    address: "Door-to-door service",
+    lat: null,
+    lng: null,
+    page: "modivcaremedicaltransportation.html",
+    distanceMode: "service"
+  },
+  {
+    id: "boys-girls-clubs-lower-bergen",
+    name: "Boys & Girls Clubs of Lower Bergen County",
+    category: "Afterschool Care",
+    address: "390 Main St, Hackensack, NJ 07601",
+    lat: 40.8865,
+    lng: -74.0437,
+    page: "boysandgirlsclubsoflowerbergencounty.html",
+    distanceMode: "fixed"
+  },
+  {
+    id: "food-brigade",
+    name: "The Food Brigade (Community FoodBank Trenton)",
+    category: "Food Safety",
+    address: "Distribution locations vary",
+    lat: null,
+    lng: null,
+    page: "thefoodbrigadecommunityfoodbanktrenton.html",
+    distanceMode: "variable"
+  }
+];
